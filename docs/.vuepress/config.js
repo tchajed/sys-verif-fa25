fa658fd61cd5ab962f2b2e7ea253488c5050865b
Tej Chajed <chajed@wisc.edu>
2024-08-05 18:58:51 -0500

    Fix base URL for GitHub pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
+  base: "/sys-verif-fa24/",
 
   title: 'CS 839',
   description: 'Systems Verification Fall 2024',
