70a85501ad26a83e8fcb36ec953bd89f2cdd52f9
Tej Chajed <chajed@wisc.edu>
2024-08-06 12:15:04 -0500

    Port vuepress config to typescript

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,50 @@
+import { hopeTheme } from "vuepress-theme-hope";
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
+
+export default defineUserConfig({
+  lang: "en-US",
+  base: "/sys-verif-fa24/",
+
+  title: "CS 839",
+  description: "Systems Verification Fall 2024",
+
+  theme: hopeTheme({
+    navbar: [
+      {
+        text: "Calendar",
+        link: "/",
+      },
+      "/assignments",
+    ],
+
+    sidebar: [
+      {
+        link: "/",
+        text: "Home",
+      },
+      "/syllabus",
+      "/assignments",
+    ],
+
+    // control page meta information shown
+    // see https://theme-hope.vuejs.press/guide/feature/meta.html
+    contributors: false,
+    editLink: false,
+    pageInfo: ["Date"],
+    toc: true, // disabled on home page
+    print: false,
+
+    plugins: {
+      mdEnhance: {
+        katex: true,
+      },
+    },
+    logo: "/logo.png",
+    favicon: "/favicon.png",
+    iconAssets: "fontawesome",
+  }),
+
+  bundler: viteBundler(),
+  host: "localhost",
+});
